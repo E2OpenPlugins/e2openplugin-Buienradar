b401f0b3bd4f72b9ac088d50f7f5077fb08f2176
Sjaaky <sjaaky@users.sourceforge.net>
2011-09-22 20:32:48 +0200

    prepare for e2openplugins

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+
+pkg = 'Extensions.Buienradar'
+setup (name = 'enigma2-plugin-extensions-buienradar',
+       version = '0.1',
+       description = 'Buienradar',
+       packages = [pkg],
+       package_dir = {pkg: 'plugin'},
+       package_data = {pkg: 
+           ['plugin.png']},
+      )
